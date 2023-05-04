8aba8b71b84e76be64fff0b8eca05c7bae137598
Diego <rijmjada@gmail.com>
2023-05-04 13:04:06 -0300

    first

---
+++ main.js
@@ -0,0 +1,7 @@
+let typingText = new Typed(".change-text", {
+    strings:["Desarrollador","Programador","Estudiante","Coder"],
+    loop: true,
+    typeSpeed: 85,
+    backSpeed: 45,
+    backDelay: 1500,
+})
